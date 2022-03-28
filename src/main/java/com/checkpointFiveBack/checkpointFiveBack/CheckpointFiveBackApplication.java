32f617e40d98bc81d5fb660f1baba527da8cabbc
Karim Boudjemai <karim@MacBook-Pro-de-Karim.local>
2022-03-28 16:39:50 +0200

    first commit

---
+++ src/main/java/com/checkpointFiveBack/checkpointFiveBack/CheckpointFiveBackApplication.java
@@ -0,0 +1,13 @@
+package com.checkpointFiveBack.checkpointFiveBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckpointFiveBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckpointFiveBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/checkpointFiveBack/checkpointFiveBack/CheckpointFiveBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.checkpointFiveBack.checkpointFiveBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckpointFiveBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
